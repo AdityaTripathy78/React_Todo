590d258cac78777e27f77fbc4944fbc512bd6a47
aditya <adityalipu78@gmail.com>
2021-10-22 07:37:47 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Todo from "./Todo";
+// import Tabs from "./component/TabMenu/Tabs";
+// import GalleryReact from "./component/TabMenu/GalleryReact";
+// import "App.css";
+
+const App = () => {
+  return (
+    <>
+      {/* <GalleryReact /> */}
+      <Todo />
+    </>
+  );
+};
+
+export default App;
